d1e487a000eb791da3735d862815b4960d6ac2ae
thrall47 <hitmanahmed444777@gmail.com>
2022-03-12 16:44:33 +0200

    v 1.0

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,13 @@
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.calculator.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
